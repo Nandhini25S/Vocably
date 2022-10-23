75a52b22bd7079723087c8e96cbc7cb0cd79a40e
Sanjaypranav <sanjaypriya195@gmail.com>
2022-10-23 12:33:01 +0530

    edited vocabularies

--- test/test_text_preprocessor.py
+++ test/test_text_preprocessor.py
@@ -1,10 +1,8 @@
-from vocably.preprocessing.text import Preprocess
+from vocably.preprocessing.text import Preprocessor
 
 
 def test_preprocess():
-    preprocess = Preprocess(remove_links=False, remove_punctuation=False, remove_stopwords=False,
-                            lemmatize=False, remove_numbers=False, nltk_tokenize=False)
+    preprocess = Preprocessor()
     text = "hello! Welcome to vocably-0.0.6 from https://pypi.org/project/vocably/"
-    assert (preprocess.tokenize(preprocess.normalise(text)) == ['hello!', 'welcom',
-                                                                'to', 'vocably-0.0.6', 'from',
-                                                                'https://pypi.org/project/vocably/'])
+    assert (preprocess.tokenize(preprocess.normalize(text)) == ['hello', 'welcome', 'to', 'vocably', 'from', 'pypi',
+                                                                'org', 'project', 'vocably'])
